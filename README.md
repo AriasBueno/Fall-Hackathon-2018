d919cc8a7bd865a09281151a4fc7c125bb436e5f
AriasBueno <madelinarias@mail.com>
2018-11-17 10:54:56 -0500

    Posting final project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Fall-Hackathon-2018
+
+It is currently difficult and sometimes impossible for the visually impaired to use a Banking Mobile App.But with My Simple Bank, they will be able to improve their Banking experience with the help of a voice assistant.
+
+There are two major benefits when using this app. Make Banking Mobile Application friendly and easy for clients with Visual impaired. Get clients to be more open to help out when others are in time of need.
+
+# How Do we make our app friendly to people with visual impairs?
+
+My Simple Bank is able to Voice Assist a client with poor or not vision with simple voice commands to execute a transaction such as:
+Responding What is Their Actual Balances in XYZ Accounts Transferring from X to Y Account A Brief history of the Last Transactions Contact Costumer Service Line if Needed
--- app.js
+++ app.js
@@ -1,39 +1,65 @@
 /* app.js */
-
 //require modules
 var express = require('express');
-\var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var urlencodedString = bodyParser.urlencoded({extended: true});
-
-
+const fs = require('fs');
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+// The text to synthesize
+const text = 'The balance in your checking account is 534.10 dollars, savings 100.10.               Your last transaction was Starbucks 5 dollars,     Amazon 25 Dollars,    stop and shop 70 dollars';
+const text3 = 'One moment please';
+const text4 = 'Succefully complete';
+// Construct the request
+const request = {
+  input: {text: text},
+  // Select the language and SSML Voice Gender (optional)
+  voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+  // Select the type of audio encoding
+  audioConfig: {audioEncoding: 'MP3'},
+};
 // var path = require('path');
 var app = express();
-
 //configure app
 app.set('view engine', 'ejs');
-
-
 //use middleware
 app.use(express.static(__dirname + '/public'));//this displays the style.css and normalize.css file
 // app.use(express.static(__dirname + '/views'));
 // app.use(express.static(__dirname + '/profile'));
 // app.use(bodyParser());
-
-
+////////////
+// Performs the Text-to-Speech request
+client.synthesizeSpeech(request, (err, response) => {
+  if (err) {
+    console.error('ERROR:', err);
+    return;
+  }
+  // Write the binary audio content to a local file
+  fs.writeFile('output.mp3', response.audioContent, 'binary', err => {
+    if (err) {
+      console.error('ERROR:', err);
+      return;
+    }
+    console.log('Audio content written to file: output.mp3');
+  });
+});
+//////////
 //define routes
 app.get('/', function(req, res){
-	res.render('home');
-	console.log('inside home page!');
+    res.render('home');
+    console.log('inside home page!');
 });
-
 app.get('/home', function(req, res){
-	res.render('home');
-
-	console.log('inside home page!');
+    res.render('home');
+    console.log('inside home page!');
 });
-
-
+app.get('/donation', function(req, res){
+    console.log('inside donation page!');
+    res.render('donations');
+});
 //displaying on web app
 app.listen(8000, function(){
-	console.log('ready on port 8000');
+    console.log('ready on port 8000');
 });
