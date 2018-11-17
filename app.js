01dbd4fc25896493357fc517f75b3a8b30e0f811
AriasBueno <madelinarias@mail.com>
2018-11-17 02:26:14 -0500

    Removed the now ignored directory node_modules

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-/* app.js */
-
-//require modules
+var createError = require('http-errors');
 var express = require('express');
-var bodyParser = require('body-parser');
-var urlencodedString = bodyParser.urlencoded({extended: true});
-
-
-// var path = require('path');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var app = express();
 
-//configure app
-app.set('view engine', 'ejs');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//use middleware
-app.use(express.static(__dirname + '/public'));//this displays the style.css and normalize.css file
-// app.use(express.static(__dirname + '/views'));
-// app.use(express.static(__dirname + '/profile'));
-// app.use(bodyParser());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-
-//define routes
-app.get('/', function(req, res){
-	res.render('home');
-	console.log('inside home page!');
-
-
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/home', function(req, res){
-	res.render('home');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	console.log('inside home page!');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-
-//displaying on web app
-app.listen(8000, function(){
-	console.log('ready on port 8000');
-});
+module.exports = app;
